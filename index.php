f643f8242d8ed9bfeaa36ce7ec27d6ad15af68d2
JennyViannay <jenny.viannay75@gmail.com>
2020-03-23 13:33:22 +0100

    fix index.php

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
 // Ecrire la fonction getLike() qui prend en paramètre un array et qui return une string
-function getLike($users)
+function getLike($users) : String
 {
     // RAPPEL : Les messages suivants doivent s'afficher 
     // selon s'il y a 0, 1, 2, ou supérieur à 2 users dans le tableaux d'entrée 
@@ -9,6 +9,7 @@ function getLike($users)
     // "Simon aime cette publication" = 1 
     // "Simon et Arthur aiment cette publication" = 2 
     // "Simon et X autres personnes ont aimé cette publication." > 2 
+    return "Ceci est une string";
 }
 
 $users = []; // Créer la data qui permet de tester la méthode
