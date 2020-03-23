892ba3b42c8cc53915d59f12e0b4bfb55dd7c0a7
JennyViannay <jenny.viannay75@gmail.com>
2020-03-23 13:26:40 +0100

    fix index.php

--- index.php
+++ index.php
@@ -1,3 +1,17 @@
 <?php
-    echo "hello";
-    
+
+// Ecrire la fonction getLike() qui prend en paramètre un array et qui return une string
+function getLike($users)
+{
+    // RAPPEL : Les messages suivants doivent s'afficher 
+    // selon s'il y a 0, 1, 2, ou supérieur à 2 users dans le tableaux d'entrée 
+    // "Soyez le premier à aimer cette publication" = 0 
+    // "Simon aime cette publication" = 1 
+    // "Simon et Arthur aiment cette publication" = 2 
+    // "Simon et X autres personnes ont aimé cette publication." > 2 
+}
+
+$users = []; // Créer la data qui permet de tester la méthode
+
+// Tester la fonction
+echo getLike($users);
