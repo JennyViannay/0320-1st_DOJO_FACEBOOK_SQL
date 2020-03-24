10fac03558bd65fddc5a76d054accc6e3f08deaf
JennyViannay <jenny.viannay75@gmail.com>
2020-03-24 11:59:15 +0100

    dojo correction

--- README.md
+++ README.md
@@ -1,3 +1,4 @@
+:) 
 ## EXERCICE 1 = Facebook - Ecrire la fonction getLike()
 ### Consignes
 #### À partir d'un tableau $users contenant une série de prénoms : 
@@ -12,7 +13,7 @@
 
 ## EXERCICE 2 = Créer la base de données permettant de stocker les valeurs contenues dans $user
 ### Consignes
-Se connecter au serveur mySql => <code>mysql -u root -p</code>
+Se connecter au serveur mySql => <code>mysql -u root -p</code> root
 - Créer la database nommée Facebook
 - Créer une Table users qui contient un champ id en auto increment primary key not null et un champ name varchar (100)
 - Insérer au moins 3 users dans la table users et afficher le résultat
@@ -23,3 +24,5 @@ Tests successifs,
 Fonctions,
 Manipulation (simple) de talbeaux,
 Intro SQL
+
+Créer une table 'Johnny vive Johnny :)'priv" de dessert johnny :p
--- index.php
+++ index.php
@@ -1,18 +1,35 @@
 <?php
 
+// count();
+
 // Ecrire la fonction getLike() qui prend en paramètre un array et qui return une string
 function getLike($users) : String
 {
     // RAPPEL : Les messages suivants doivent s'afficher 
     // selon s'il y a 0, 1, 2, ou supérieur à 2 users dans le tableaux d'entrée 
-    // "Soyez le premier à aimer cette publication" = 0 
-    // "Simon aime cette publication" = 1 
+    // OK "Soyez le premier à aimer cette publication" = 0 
+    // OK "Simon aime cette publication" = 1 
+    // ---------------------------------------------------
     // "Simon et Arthur aiment cette publication" = 2 
     // "Simon et X autres personnes ont aimé cette publication." > 2 
-    return "Ceci est une string";
+    $nbUsers = count($users);
+    if($nbUsers == 0) {
+        return "Soyez le premier à aimer cette publication";
+    }
+    elseif($nbUsers ==1) {
+        return  $users[0]." aime cette publication";
+    }
+    elseif($nbUsers ==2) {
+        return  $users[0]. " et " .$users[1] ." aiment cette publication";
+    }
+    else {
+        // array_rand($users) est égale à un index du tableau $users
+        // écrire $users[array_rand($users)] revient au même que d'écrire $users[INDEX];
+        return  $users[array_rand($users)]. " et " .($nbUsers - 1) ." aiment cette publication";
+    }
 }
 
-$users = []; // Créer la data qui permet de tester la méthode
+$users = ['Sten','Sandrine','Lisa','Nico','Vara']; // Créer la data qui permet de tester la méthode
 
 // Tester la fonction
 echo getLike($users);
